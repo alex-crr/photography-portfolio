f7fd48284026a6227ccfdd76e18d6a73a3a5277a
alex-crr <73464112+alex-crr@users.noreply.github.com>
2026-04-29 22:41:23 +0200

    Convert fulls to AVIF (q55) and keep thumbs as JPEG for EXIF compatibility

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -25,9 +25,9 @@ gulp.task('delete', function () {
 gulp.task('resize-images', async function () {
     const files = readdirSync('images').filter(f => /\.(jpe?g|png|webp|tiff?)$/i.test(f));
     for (const filename of files) {
-        const outName = filename.replace(/^_+/, '').replace(/[\s()]+/g, '_');
-        await sharp(`images/${filename}`).resize(2500).withMetadata().toFile(`images/fulls/${outName}`);
-        await sharp(`images/${filename}`).resize(512).withMetadata().toFile(`images/thumbs/${outName}`);
+        const baseName = filename.replace(/^_+/, '').replace(/[\s()]+/g, '_').replace(/\.[^.]+$/, '');
+        await sharp(`images/${filename}`).resize(2500).withMetadata().toFormat('avif', { quality: 55 }).toFile(`images/fulls/${baseName}.avif`);
+        await sharp(`images/${filename}`).resize(512).withMetadata().toFormat('jpeg', { quality: 85 }).toFile(`images/thumbs/${baseName}.jpg`);
     }
 });
 
